e584695680978ed1d00ddd8e5fce59d47b1a19d3
liuuu <next@yudeMacBook-Air.local>
2017-06-01 07:04:08 +0800

    single component & scoped style

--- clione/vuejs-playlist/src/App.vue
+++ clione/vuejs-playlist/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    
     <button v-on:click="greeting()">greet</button>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from './Ninjas.vue'
+
+
 export default {
+  components: {
+    'ninjas': Ninjas
+  },
   data () {
     return {
-      title: 'Your first Vue file',
+      title: 'Ninja App',
     }
   },
   methods: {
@@ -21,31 +27,8 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped>
+  h1 {
+    color: purple;
+  }
 </style>
