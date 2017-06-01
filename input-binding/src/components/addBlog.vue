657c27340dc4d3b50672ef886f1fcfbc38290b2b
liuuu <next@yudeMacBook-Air.local>
2017-06-01 20:35:33 +0800

    input binding

---
+++ input-binding/src/components/addBlog.vue
@@ -0,0 +1,91 @@
+<template id="">
+  <div id="add-blog">
+    <h2>Add a new blog</h2>
+    <form  method="post">
+      <label for="">Blog Title:</label>
+      <input type="text" v-model="blog.title">
+      <label for="">Blog Content</label>
+      <textarea v-model="blog.content"></textarea>
+      <div id="checkbox">
+        <label for="">categories:</label>
+        <label for="">ninjas</label>
+        <input type="checkbox" name="" value="ninjas" v-model="blog.categories">
+        <label for="">wizards</label>
+        <input type="checkbox" name="" value="wizards" v-model="blog.categories">
+        <label for="">mario</label>
+        <input type="checkbox" name="" value="mario" v-model="blog.categories">
+        <label for="">cheese</label>
+        <input type="checkbox" name="" value="cheese" v-model="blog.categories">
+      </div>
+    </form>
+    <label for="">Authors:</label>
+    <select class="" name="" v-model="blog.author">
+      <option v-for="author in authors">{{author}}</option>
+    </select>
+    <div id="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog title:{{blog.title}}</p>
+      <p>Blog content:</p>
+      <p>{{blog.content}}</p>
+      <p>Blog categories:</p>
+      <ul>
+        <li v-for="category in blog.categories">{{category}}</li>
+      </ul>
+      <p>Author:</p>
+      <p>{{blog.author}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// Imports
+export default {
+    data () {
+        return {
+            blog: {
+                title: '',
+                content: '',
+                categories:[],
+                author:''
+            },
+            authors:['lee', 'ninja', 'panda']
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+#add-blog *{
+    box-sizing: border-box;
+}
+#add-blog{
+    margin: 20px auto;
+    max-width: 500px;
+}
+label{
+    display: block;
+    margin: 20px 0 10px;
+}
+input[type="text"], textarea{
+    display: block;
+    width: 100%;
+    padding: 8px;
+}
+#preview{
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+}
+h3{
+    margin-top: 10px;
+}
+#checkbox input{
+  display: inline-block;
+  margin-right: 10px;
+}
+#checkbox label {
+  display: inline-block;
+}
+</style>
